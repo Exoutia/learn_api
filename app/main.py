b62470813d99b3b83e4d8cbb67b589d523b533bd
exoutia <jhav365@gmail.com>
2023-10-28 22:33:48 +0530

    creating requirements.txt for the file adding some things for alembic migrations.

--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from . import models
-from .database import engine
 from .routers import auth, posts, users, votes
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
+# this is  not needed now as we are using alembic for migrations
 
+origins = ["*"] 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(posts.router)
 app.include_router(users.router)
@@ -18,6 +27,3 @@ app.include_router(votes.router)
 @app.get("/")
 def say_hello():
     return {"message": "post app"}
-
-
-# this is a test comment
